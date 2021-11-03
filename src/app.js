e6d1522a4c90e0b615e5bde671c2291e254c7d5e
Jason Ostergren <jason@Jasons-MacBook-Pro.local>
2021-11-02 20:09:51 -0500

    updated reactchat app

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './app.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="dd636b30-aa8d-410a-990b-c0f444fd6df8"
+            userName="joste"
+            userSecret="Danniella@27"
+        />
+    );
+}
+
+export default App;
