273ee73d580ba903b7ac5d58eda4a3ec8b0f5b75
Jason Ostergren <jason@Jasons-MacBook-Pro.local>
2021-11-03 19:10:43 -0500

    updated ClamChat with initial work on ChatFeed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from "./components/ChatFeed";
+
 import './app.css';
 
 const App = () => {
@@ -9,6 +11,7 @@ const App = () => {
             projectID="dd636b30-aa8d-410a-990b-c0f444fd6df8"
             userName="joste"
             userSecret="Danniella@27"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
         />
     );
 }
